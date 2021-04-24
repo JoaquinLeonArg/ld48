0342214a514ca2e338e6baf186120be0aac44374
Joaquin Leon <leonjoaquin77@gmail.com>
2021-04-24 15:26:59 -0300

    Added items and pickups

--- Components/Floors/Floor.gd
+++ Components/Floors/Floor.gd
@@ -1,8 +1,12 @@
 extends Area2D
+class_name Floor
 
 var spinning: bool = false
 onready var base_position: Vector2 = $Sprite.position
-	
+
+func _ready():
+	GameState.floor_tiles.append(self)
+
 func _process(delta):
 	if self.spinning:
 		$Sprite.position = self.base_position + Vector2(randf()*5 - 1, randf()*5 - 1)
--- Globals/GameState.gd
+++ Globals/GameState.gd
@@ -1,16 +1,16 @@
 extends Node
 
+var player = null
+var inventory = null
+var floor_tiles = []
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var destroy_timer = 0
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	self.destroy_timer += delta
+	if self.destroy_timer > 5:
+		
+		self.destroy_timer -= 5
+		var i = randi() % len(self.floor_tiles)
+		self.floor_tiles[i].spin_and_destroy()
+		self.floor_tiles.remove(i)
