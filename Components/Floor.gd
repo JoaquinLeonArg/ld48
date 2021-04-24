4a7e1a573470c81511947b2ddadbc3800a6298fb
Joaquin Leon <leonjoaquin77@gmail.com>
2021-04-24 19:50:02 -0300

    Something

--- Components/Floor.gd
+++ Components/Floor.gd
@@ -5,7 +5,7 @@ onready var base_position: Vector2 = $Sprite.position
 	
 func _process(delta):
 	if self.spinning:
-		$Sprite.position = self.base_position + Vector2(randf()*5 - 1, randf()*5 - 1)
+		$Sprite.position = self.base_position + Vector2(randf()*2 - 1, randf()*2 - 1)
 	
 func spin_and_destroy():
 	self.spinning = true
--- Globals/GameState.gd
+++ Globals/GameState.gd
@@ -1,5 +1,6 @@
 extends Node
 
+<<<<<<< Updated upstream
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -14,3 +15,22 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+=======
+var player = null
+var inventory = null
+var floor_tiles = []
+
+var destroy_timer = 0
+var damage_factor = 1
+
+func _process(delta):
+	self._destroy_floor(delta)
+
+func _destroy_floor(delta):
+	self.destroy_timer += delta*self.damage_factor
+	if self.destroy_timer > 2 and self.floor_tiles:
+		self.destroy_timer -= 2
+		var i = randi() % len(self.floor_tiles)
+		self.floor_tiles[i].spin_and_destroy()
+		self.floor_tiles.remove(i)
+>>>>>>> Stashed changes
