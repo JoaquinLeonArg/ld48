e85f37fe43de2982f462bacc8c19e33d62388407
Joaquin Leon <leonjoaquin77@gmail.com>
2021-04-24 02:06:48 -0300

    Basic item usage

--- Components/Camera.gd
+++ Components/Camera.gd
@@ -10,7 +10,6 @@ var current_offset = Vector2.ZERO
 
 func _process(delta):
 	self.target_offset = get_viewport().get_mouse_position() / get_viewport().size - Vector2(0.5, 0.5)
-	print(self.current_offset)
 	self.current_offset = self.current_offset.move_toward(self.target_offset, delta*camera_speed*current_offset.distance_squared_to(target_offset))
 	if self.tracked_object:
 		self.position = get_node(self.tracked_object).position + self.current_offset*camera_distance
